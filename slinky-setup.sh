4e5de83e597725e3a6134057bee8c231357e2b1d
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-10-16 03:49:55 +0300

    Update slinky-setup.sh

--- slinky-setup.sh
+++ slinky-setup.sh
@@ -20,6 +20,22 @@ echo 'export PORT='$PORT
 echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
+# Go kurulumunu gerçekleştirme
+echo -e "${GREEN}Installing Go...${NC}"
+cd $HOME
+VER="1.23.0"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Versiyon kontrolü
+echo $(go version) && sleep 1
+
 # Start the installation of Slinky
 echo -e "${GREEN}Starting Slinky installation...${NC}" && sleep 1
 cd $HOME
