cf12afe866abbecaf46cd2082aa67b24f2425cba
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-10-16 03:23:57 +0300

    Add files via upload

---
+++ mictonode_setup.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+# MICTONODE ASCII logosunu gösterme fonksiyonu
+function printMictoNode {
+  echo -e "${GREEN}"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@=======================================================================================+@"
+  echo "@ =%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                           .@"
+  echo "@ +@@@*******@@@@@@@@@@@@@@@@@@@@@%******#@@@      .:::::::                 .:::::.     .@"
+  echo "@ +@@#       :%@@@@@@@@@@@@@@@@@@*       .@@@      -@@@@@@@@=               =@@@@@+     .@"
+  echo "@ +@@#         *@@@@@@@@@@@@@@@@-        .@@@      -@@@@@@@@@%-             =@@@@@+     .@"
+  echo "@ +@@#          -@@@@@@@@@@@@@#.         .@@@      -@@@@@@@@@@@#.           =@@@@@+     .@"
+  echo "@ +@@#           .#@@@@@@@@@@=           .@@@      -@@@@@%@@@@@@@*.         =@@@@@+     .@"
+  echo "@ +@@#      .      +@@@@@@@%:      .     .@@@      -@@@@@* +@@@@@@@=        =@@@@@+     .@"
+  echo "@ +@@#      @=      :%@@@@*       #*     .@@@      -@@@@@*  .*@@@@@@%-      =@@@@@+     .@"
+  echo "@ +@@#      @@*       *@@-      :@@*     .@@@      -@@@@@*    :#@@@@@@%:    =@@@@@+     .@"
+  echo "@ +@@#      @@@%:      -.      +@@@*     .@@@      -@@@@@*      -%@@@@@@*.  =@@@@@+     .@"
+  echo "@ +@@#      @@@@@+           .%@@@@*     .@@@      -@@@@@*        =@@@@@@@+ =@@@@@+     .@"
+  echo "@ +@@#      @@@@@@%.        =@@@@@@*     .@@@      -@@@@@*          *@@@@@@@#@@@@@+     .@"
+  echo "@ +@@#      @@@@@@@@-      *@@@@@@@*     .@@@      -@@@@@*           .#@@@@@@@@@@@+     .@"
+  echo "@ +@@#      @@@@@@@@@%%%%%@@@@@@@@@*     .@@@      -@@@@@*             :%@@@@@@@@@+     .@"
+  echo "@ +@@#      @@@@@@@@@@@@@@@@@@@@@@@*     .@@@      -@@@@@*               =@@@@@@@@+     .@"
+  echo "@ +@@@+++++*@@@@@@@@@@@@@@@@@@@@@@@%+++++*@@@      .-----:                 -------:     .@"
+  echo "@ +%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                           .@"
+  echo "@========================================================================================@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  echo -e "${NC}"
+}
+
+# MICTONODE logosunu göster
+printMictoNode
+
+# Renkli çıktı için yardımcı fonksiyonlar
+function printGreen {
+  echo -e "${GREEN}${1}${NC}"
+}
+
+# Paket ve bağımlılıkların yüklenmesi
+printGreen "1. Updating and installing dependencies..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# kullanıcı girdileri
+read -p "Enter WALLET name:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit) :" PORT
+echo 'export PORT='$PORT
+
+# varyasyonları ayarlama
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export WARDEN_CHAIN_ID="buenavista-1"" >> $HOME/.bash_profile
+echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$WARDEN_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$WARDEN_PORT\e[0m"
+printLine
+sleep 1
+
+# Go kurulumunu gerçekleştirme
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.21.6"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Versiyon kontrolü
+echo $(go version) && sleep 1
+
+# Warden protocol binary kurulum
+printGreen "3. Downloading Warden binary and setting up..." && sleep 1
+mkdir -p $HOME/.warden/cosmovisor/genesis/bin
+wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.4.2/wardend_Linux_x86_64.zip
+unzip -o wardend_Linux_x86_64.zip
+rm -rf wardend_Linux_x86_64.zip
+chmod +x wardend
+mv wardend $HOME/.warden/cosmovisor/genesis/bin/
+
+# Cosmovisor ve binary için sembolik linkler oluşturma
+sudo ln -s $HOME/.warden/cosmovisor/genesis $HOME/.warden/cosmovisor/current -f
+sudo ln -s $HOME/.warden/cosmovisor/current/bin/wardend /usr/local/bin/wardend -f
+
+# Cosmovisor kurulumu
+printGreen "4. Installing Cosmovisor..." && sleep 1
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+
+# Servis dosyası oluşturma
+printGreen "5. Creating service file..." && sleep 1
+sudo tee /etc/systemd/system/wardend.service > /dev/null << EOF
+[Unit]
+Description=warden node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.warden"
+Environment="DAEMON_NAME=wardend"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.warden/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Servis aktif etme
+sudo systemctl daemon-reload
+sudo systemctl enable wardend
+
+# Node başlatma ve yapılandırma
+printGreen "6. Initializing the node..."
+wardend config set client chain-id ${WARDEN_CHAIN_ID}
+wardend config set client keyring-backend test
+wardend config set client node tcp://localhost:${WARDEN_PORT}657
+wardend init ${MONIKER} --chain-id ${WARDEN_CHAIN_ID}
+sed -i -e "s|^node *=.*|node = \"tcp://localhost:${WARDEN_PORT}657\"|" $HOME/.warden/config/client.toml
+
+# Genesis ve Addrbook dosyalarının indirilmesi
+printGreen "7. Downloading genesis and addrbook..."
+curl -Ls https://snapshots.kjnodes.com/warden-testnet/genesis.json > $HOME/.warden/config/genesis.json
+wget -O $HOME/.warden/config/addrbook.json "https://raw.githubusercontent.com/MictoNode/warden-protocol/main/addrbook.json"
+
+# Gas fiyatları ve port ayarları
+printGreen "8. Configuring custom ports and gas prices..." && sleep 1
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025uward\"/" ~/.warden/config/app.toml
+sed -i.bak -e "s%:1317%:${WARDEN_PORT}317%g;
+s%:8080%:${WARDEN_PORT}080%g;
+s%:9090%:${WARDEN_PORT}090%g;
+s%:9091%:${WARDEN_PORT}091%g;
+s%:8545%:${WARDEN_PORT}545%g;
+s%:8546%:${WARDEN_PORT}546%g;
+s%:6065%:${WARDEN_PORT}065%g" $HOME/.warden/config/app.toml
+
+# P2P ve port yapılandırması
+sed -i.bak -e "s%:26658%:${WARDEN_PORT}658%g;
+s%:26657%:${WARDEN_PORT}657%g;
+s%:6060%:${WARDEN_PORT}060%g;
+s%:26656%:${WARDEN_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${WARDEN_PORT}656\"%;
+s%:26660%:${WARDEN_PORT}660%g" $HOME/.warden/config/config.toml
+
+# Seed ve peer yapılandırması
+printGreen "9. Setting up peers and seeds..." && sleep 1
+SEEDS="8288657cb2ba075f600911685670517d18f54f3b@warden-testnet-seed.itrocket.net:18656"
+PEERS="b14f35c07c1b2e58c4a1c1727c89a5933739eeea@warden-testnet-rpc.itrocket.net:18656,88806b3c6e081b26a5ab6fd0eda11c51e5f31bdf@37.120.189.81:11256,e850365a8232650623f30356c77583939b896327@116.202.217.20:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@warden-testnet.rpc.kjnodes.com:17856,85abfb1a10ef88d37277e7462830890ff2f7a1ac@warden-testnet-rpc.cryptonode.id:24656,8fa48bbd20d316382f339bd00f31d3a2678682d2@144.76.29.90:26656,67b61778e63da8a9bc8083eb465077ba731940c4@88.198.50.206:36656,eb2e7095f86b24e8d5d286360c34e060a8db6334@188.40.85.207:12756,e590018206d240cfdc235ca7f76b386da31fe5d3@82.223.31.166:26652,0bd82efac906de0187072de528c0c0f0634e89ca@95.216.248.117:46656,a13ef502d7e3884c346128450731504d2fef85a5@195.201.195.61:41656,e9cdb4b9c694b86a1e6f0800e9dd0457c5277767@158.220.102.69:26656,91f8ec7104a62db9743d1ed2b03fd49ef6e2f75b@65.108.78.101:12756,78911898af71d4addb033027bb38a64ab1a5873b@88.99.137.138:42256,8fbb05e30cdd527b1752d63fbc7f714c69cc76b6@144.76.112.58:35656,877b5f0a95e52938f02ecd555f1a3a11ebdf2df0@65.108.72.233:56656,878b2a5a2632b868f0890cf995614d6c6b43ef99@65.109.84.33:27356,bc5b3a95135b1c980b7d61e63c5be21df2c90a95@37.27.124.38:46656,8c934a899518b4a1eb8975eb452d3cc1a8c2ed13@warden-testnet-rpc.stake-town.com:39656,5d001600956ba67136c17baa8ff2da0b074b8364@rpc.warden.aknodes.net:16256"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.warden/config/config.toml
+
+# Pruning ayarları
+printGreen "10. Configuring pruning..." && sleep 1
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" ~/.warden/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" ~/.warden/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" ~/.warden/config/app.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" ~/.warden/config/config.toml
+
+# Snapshot indirme ve node başlatma
+printGreen "11. Downloading snapshot and starting node..." && sleep 1
+wardend tendermint unsafe-reset-all --home $HOME/.warden
+if curl -s --head curl https://snapshots.kjnodes.com/warden-testnet/snapshot_latest.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://snapshots.kjnodes.com/warden-testnet/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.warden
+else
+  echo "No snapshot available"
+fi
+
+# Servisi başlatma
+sudo systemctl daemon-reload
+sudo systemctl restart wardend
+sudo journalctl -u wardend -f -o cat
---
+++ slinky-setup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Color definitions (for colored terminal output)
+GREEN="\e[1m\e[1;32m"
+NC="\e[0m"
+
+# Get PORT information from the user
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Add the PORT information to the .bash_profile
+echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+# Start the installation of Slinky
+echo -e "${GREEN}Starting Slinky installation...${NC}" && sleep 1
+cd $HOME
+rm -rf slinky
+git clone https://github.com/skip-mev/slinky.git
+cd slinky
+git checkout v1.0.5
+make build
+sudo mv build/slinky /usr/local/bin/
+
+# Create the Slinky service file
+echo -e "${GREEN}Creating the Slinky service file...${NC}"
+sudo tee /etc/systemd/system/slinkyd.service > /dev/null <<EOF
+[Unit]
+Description=Warden Slinky Oracle
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which slinky) --market-map-endpoint 127.0.0.1:${WARDEN_PORT}090
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start the services
+echo -e "${GREEN}Starting the Slinky service...${NC}" && sleep 1
+sudo systemctl daemon-reload
+sudo systemctl enable slinkyd
+sudo systemctl start slinkyd
+
+# Show Slinky logs
+echo -e "${GREEN}Displaying Slinky logs...${NC}" && sleep 1
+journalctl -fu slinkyd --no-hostname &
+
+# Instruction message
+print_instructions() {
+    echo -e "${GREEN}Please make sure that the following settings are in your 'app.toml' file:${NC}"
+    echo "###############################################################################"
+    echo "###                                  Oracle                                 ###"
+    echo "###############################################################################"
+    echo "[oracle]"
+    echo "# Enabled indicates whether the oracle is enabled."
+    echo 'enabled = "true"'
+    echo ""
+    echo "# Oracle Address is the URL of the out-of-process oracle sidecar. This is used to"
+    echo "# connect to the oracle sidecar when the application boots up. Note that the address"
+    echo "# can be modified at any point, but will only take effect after the application is"
+    echo "# restarted. This can be the address of an oracle container running on the same"
+    echo "# machine or a remote machine."
+    echo 'oracle_address = "127.0.0.1:8080"'
+    echo ""
+    echo "# Client Timeout is the time that the client is willing to wait for responses from"
+    echo "# the oracle before timing out."
+    echo 'client_timeout = "2s"'
+    echo ""
+    echo "# MetricsEnabled determines whether oracle metrics are enabled. Specifically"
+    echo "# this enables instrumentation of the oracle client and the interaction between"
+    echo "# the oracle and the app."
+    echo 'metrics_enabled = "true"'
+    echo ""
+    echo -e "${GREEN}Then, restart the Warden and Slinky services:${NC}"
+    echo "1. Restart the services:"
+    echo "   sudo systemctl daemon-reload && sudo systemctl restart wardend && sudo systemctl restart slinkyd"
+    echo ""
+    echo "To view the service logs:"
+    echo "   journalctl -fu slinkyd --no-hostname"
+}
+
+# Display instructions to the user
+print_instructions
+
+# Ask the user if they have completed the settings
+read -p "Have you completed the settings? (Y/N): " answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    # Restart the services
+    echo -e "${GREEN}Restarting the services...${NC}" && sleep 1
+    sudo systemctl daemon-reload && sudo systemctl restart wardend && sudo systemctl restart slinkyd
+    
+    # Show Slinky logs
+    echo -e "${GREEN}Displaying Slinky logs...${NC}"
+    journalctl -fu slinkyd --no-hostname
+else
+    echo -e "${GREEN}Please complete the settings and run the script again.${NC}" && sleep 1
+    exit 1
+fi
