a141e4876e9782546d5e161f528912f486a164a7
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-10-24 08:39:40 +0300

    Update mictonode_setup.sh

--- mictonode_setup.sh
+++ mictonode_setup.sh
@@ -72,13 +72,13 @@ read -p "Enter WALLET name:" WALLET
 echo 'export WALLET='$WALLET
 read -p "Enter your MONIKER :" MONIKER
 echo 'export MONIKER='$MONIKER
-read -p "Enter your PORT (2-digit) :" PORT
+read -p "Enter your PORT (3-digit) :" PORT
 echo 'export PORT='$PORT
 
 # varyasyonları ayarlama
 echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
 echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
-echo "export WARDEN_CHAIN_ID="buenavista-1"" >> $HOME/.bash_profile
+echo "export WARDEN_CHAIN_ID="chiado_10010-1"" >> $HOME/.bash_profile
 echo "export WARDEN_PORT="$PORT"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
@@ -108,12 +108,14 @@ echo $(go version) && sleep 1
 
 # Warden protocol binary kurulum
 printGreen "3. Downloading Warden binary and setting up..." && sleep 1
-mkdir -p $HOME/.warden/cosmovisor/genesis/bin
-wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.4.2/wardend_Linux_x86_64.zip
-unzip -o wardend_Linux_x86_64.zip
-rm -rf wardend_Linux_x86_64.zip
+cd $HOME
+mkdir -p $HOME/.warden/cosmovisor/genesis/bin/
+wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.5.2/wardend_Linux_x86_64.zip
+unzip wardend_Linux_x86_64.zip
 chmod +x wardend
-mv wardend $HOME/.warden/cosmovisor/genesis/bin/
+rm -rf wardend_Linux_x86_64.zip
+cd $HOME
+mv $HOME/wardend $HOME/.warden/cosmovisor/genesis/bin/
 
 # Cosmovisor ve binary için sembolik linkler oluşturma
 sudo ln -s $HOME/.warden/cosmovisor/genesis $HOME/.warden/cosmovisor/current -f
@@ -153,54 +155,54 @@ sudo systemctl enable wardend
 printGreen "6. Initializing the node..."
 wardend config set client chain-id ${WARDEN_CHAIN_ID}
 wardend config set client keyring-backend test
-wardend config set client node tcp://localhost:${WARDEN_PORT}657
+wardend config set client node tcp://localhost:${W_PORT}57
 wardend init ${MONIKER} --chain-id ${WARDEN_CHAIN_ID}
-sed -i -e "s|^node *=.*|node = \"tcp://localhost:${WARDEN_PORT}657\"|" $HOME/.warden/config/client.toml
 
 # Genesis ve Addrbook dosyalarının indirilmesi
 printGreen "7. Downloading genesis and addrbook..."
-curl -Ls https://snapshots.kjnodes.com/warden-testnet/genesis.json > $HOME/.warden/config/genesis.json
-wget -O $HOME/.warden/config/addrbook.json "https://raw.githubusercontent.com/MictoNode/warden-protocol/main/addrbook.json"
+wget -O $HOME/.warden/config/genesis.json "https://raw.githubusercontent.com/MictoNode/warden-protocol/refs/heads/main/genesis.json"
+wget -O $HOME/.warden/config/addrbook.json "https://raw.githubusercontent.com/MictoNode/warden-protocol/refs/heads/main/addrbook.json"
 
 # Gas fiyatları ve port ayarları
 printGreen "8. Configuring custom ports and gas prices..." && sleep 1
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025uward\"/" ~/.warden/config/app.toml
-sed -i.bak -e "s%:1317%:${WARDEN_PORT}317%g;
-s%:8080%:${WARDEN_PORT}080%g;
-s%:9090%:${WARDEN_PORT}090%g;
-s%:9091%:${WARDEN_PORT}091%g;
-s%:8545%:${WARDEN_PORT}545%g;
-s%:8546%:${WARDEN_PORT}546%g;
-s%:6065%:${WARDEN_PORT}065%g" $HOME/.warden/config/app.toml
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "25000000award"|g' $HOME/.warden/config/app.toml
+sed -i.bak -e "s%:1317%:${W_PORT}17%g;
+s%:8080%:${W_PORT}80%g;
+s%:9090%:${W_PORT}90%g;
+s%:9091%:${W_PORT}91%g;
+s%:8545%:${W_PORT}45%g;
+s%:8546%:${W_PORT}46%g;
+s%:6065%:${W_PORT}65%g" $HOME/.warden/config/app.toml
 
 # P2P ve port yapılandırması
-sed -i.bak -e "s%:26658%:${WARDEN_PORT}658%g;
-s%:26657%:${WARDEN_PORT}657%g;
-s%:6060%:${WARDEN_PORT}060%g;
-s%:26656%:${WARDEN_PORT}656%g;
-s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${WARDEN_PORT}656\"%;
-s%:26660%:${WARDEN_PORT}660%g" $HOME/.warden/config/config.toml
+sed -i.bak -e "s%:26658%:${W_PORT}658%g;
+s%:26657%:${W_PORT}57%g;
+s%:6060%:${W_PORT}60%g;
+s%:26656%:${W_PORT}56%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${W_PORT}56\"%;
+s%:26660%:${W_PORT}60%g" $HOME/.warden/config/config.toml
 
 # Seed ve peer yapılandırması
 printGreen "9. Setting up peers and seeds..." && sleep 1
-SEEDS="8288657cb2ba075f600911685670517d18f54f3b@warden-testnet-seed.itrocket.net:18656"
-PEERS="b14f35c07c1b2e58c4a1c1727c89a5933739eeea@warden-testnet-rpc.itrocket.net:18656,88806b3c6e081b26a5ab6fd0eda11c51e5f31bdf@37.120.189.81:11256,e850365a8232650623f30356c77583939b896327@116.202.217.20:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@warden-testnet.rpc.kjnodes.com:17856,85abfb1a10ef88d37277e7462830890ff2f7a1ac@warden-testnet-rpc.cryptonode.id:24656,8fa48bbd20d316382f339bd00f31d3a2678682d2@144.76.29.90:26656,67b61778e63da8a9bc8083eb465077ba731940c4@88.198.50.206:36656,eb2e7095f86b24e8d5d286360c34e060a8db6334@188.40.85.207:12756,e590018206d240cfdc235ca7f76b386da31fe5d3@82.223.31.166:26652,0bd82efac906de0187072de528c0c0f0634e89ca@95.216.248.117:46656,a13ef502d7e3884c346128450731504d2fef85a5@195.201.195.61:41656,e9cdb4b9c694b86a1e6f0800e9dd0457c5277767@158.220.102.69:26656,91f8ec7104a62db9743d1ed2b03fd49ef6e2f75b@65.108.78.101:12756,78911898af71d4addb033027bb38a64ab1a5873b@88.99.137.138:42256,8fbb05e30cdd527b1752d63fbc7f714c69cc76b6@144.76.112.58:35656,877b5f0a95e52938f02ecd555f1a3a11ebdf2df0@65.108.72.233:56656,878b2a5a2632b868f0890cf995614d6c6b43ef99@65.109.84.33:27356,bc5b3a95135b1c980b7d61e63c5be21df2c90a95@37.27.124.38:46656,8c934a899518b4a1eb8975eb452d3cc1a8c2ed13@warden-testnet-rpc.stake-town.com:39656,5d001600956ba67136c17baa8ff2da0b074b8364@rpc.warden.aknodes.net:16256"
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.warden/config/config.toml
+SEEDS="2d2c7af1c2d28408f437aef3d034087f40b85401@52.51.132.79:26656"
+PEERS="7b26de79a9d13e74987d1053055f1c88502ec852@warden-chiado-peers.mictonode.com:11956,2d2c7af1c2d28408f437aef3d034087f40b85401@52.51.132.79:26656,fcaffd41eb7e3647fa953607449ff5e371c236b8@195.26.245.67:31656,5461e7642520a1f8427ffaa57f9d39cf345fcd47@54.72.190.0:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.warden/config/config.toml
 
 # Pruning ayarları
 printGreen "10. Configuring pruning..." && sleep 1
-sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" ~/.warden/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" ~/.warden/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" ~/.warden/config/app.toml
-sed -i -e "s/^indexer *=.*/indexer = \"null\"/" ~/.warden/config/config.toml
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" $HOME/.warden/config/app.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.warden/config/config.toml
 
 # Snapshot indirme ve node başlatma
 printGreen "11. Downloading snapshot and starting node..." && sleep 1
 wardend tendermint unsafe-reset-all --home $HOME/.warden
-if curl -s --head curl https://snapshots.kjnodes.com/warden-testnet/snapshot_latest.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
-  curl https://snapshots.kjnodes.com/warden-testnet/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.warden
-else
-  echo "No snapshot available"
+if curl -s --head curl https://snapshots-testnet.unitynodes.com/warden-testnet/warden-testnet-latest.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://snapshots-testnet.unitynodes.com/warden-testnet/warden-testnet-latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.warden
+    else
+  echo "no snapshot founded"
 fi
 
 # Servisi başlatma
