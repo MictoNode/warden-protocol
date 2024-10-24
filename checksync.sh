430adb185571a6aa78953ca96ba65d64b1f18a2c
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-10-24 08:41:36 +0300

    Update checksync.sh

--- checksync.sh
+++ checksync.sh
@@ -10,7 +10,7 @@ printLine() {
 
 while true; do
   local_height=$(wardend status | jq -r '.sync_info.latest_block_height')
-  network_height=$(curl -s https://warden-testnet-rpc.mictonode.com/status | jq -r '.result.sync_info.latest_block_height')
+  network_height=$(curl -s https://warden-chiado-rpc.mictonode.com/status | jq -r '.result.sync_info.latest_block_height')
   blocks_left=$((network_height - local_height))
 
   echo -e "\033[1;38mYour node height:\033[0m \033[1;34m$local_height\033[0m | \033[1;35mNetwork height:\033[0m \033[1;36m$network_height\033[0m | \033[1;29mBlocks left:\033[0m \033[1;31m$blocks_left\033[0m"
