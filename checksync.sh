d05741bb6136c56b2ccc4db58e76b9e989d54585
micto <94179917+MictoNode@users.noreply.github.com>
2024-10-24 14:26:24 +0300

    Update checksync.sh

--- checksync.sh
+++ checksync.sh
@@ -13,6 +13,10 @@ while true; do
   network_height=$(curl -s https://warden-chiado-rpc.mictonode.com/status | jq -r '.result.sync_info.latest_block_height')
   blocks_left=$((network_height - local_height))
 
+  printLine
+  printGreen "Node Sync Status"
   echo -e "\033[1;38mYour node height:\033[0m \033[1;34m$local_height\033[0m | \033[1;35mNetwork height:\033[0m \033[1;36m$network_height\033[0m | \033[1;29mBlocks left:\033[0m \033[1;31m$blocks_left\033[0m"
+  printLine
+
   sleep 5
 done
